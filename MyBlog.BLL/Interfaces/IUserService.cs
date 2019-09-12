3168017cd6373a2a4af7511e13fac6a902f4d033
Yigit Haciefendioglu <yigith1@gmail.com>
2019-09-12 12:49:18 +0300

    UserService added.

---
+++ MyBlog.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using MyBlog.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        User GetUserById(int id);
+        User GetUserByUsername(string username);
+        void AddUser(User user, string password);
+        void UpdateUser(User user);
+        void UpdatePassword(int userId, string password);
+        void DeleteUser(int id);
+        bool VerifyPassword(User user, string password);
+    }
+}
---
+++ MyBlog.BLL/Services/UserService.cs
@@ -0,0 +1,71 @@
+﻿using MyBlog.BLL.Interfaces;
+using MyBlog.Common.Security;
+using MyBlog.DAL.Contexts;
+using MyBlog.DAL.Repositories;
+using MyBlog.DAL.UnitOfWork;
+using MyBlog.Domain.Entities;
+using MyBlog.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBlog.BLL.Services
+{
+    public class UserService : IUserService
+    {
+        IUnitOfWork<MyBlogContext> _unitOfWork;
+        IRepository<User> _userRepository;
+        public UserService(IUnitOfWork<MyBlogContext> unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _userRepository = _unitOfWork.GetRepository<User>();
+        }
+
+        public void AddUser(User user, string password)
+        {
+            user.PasswordHash = HashHelper.HashPassword(password);
+            _userRepository.Add(user);
+            _unitOfWork.SaveChanges();
+        }
+
+        public void DeleteUser(int id)
+        {
+            var user = _userRepository.GetById(id);
+            _userRepository.Delete(user);
+            _unitOfWork.SaveChanges();
+        }
+
+        public User GetUserById(int id)
+        {
+            return _userRepository.GetById(id);
+        }
+
+        public User GetUserByUsername(string username)
+        {
+            return _userRepository.Get(x => x.UserName == username);
+        }
+
+        public void UpdatePassword(int userId, string password)
+        {
+            User user = _userRepository.GetById(userId);
+            user.PasswordHash = HashHelper.HashPassword(password);
+            _unitOfWork.SaveChanges();
+        }
+
+        public void UpdateUser(User user)
+        {
+            User record = _userRepository.GetById(user.Id);
+            record.Email = user.Email;
+            record.UserType = user.UserType;
+            record.UserName = user.UserName;
+
+            _userRepository.Update(record);
+            _unitOfWork.SaveChanges();
+        }
+
+        public bool VerifyPassword(User user, string password)
+        {
+            return HashHelper.VerifyHashedPassword(user.PasswordHash, password);
+        }
+    }
+}
