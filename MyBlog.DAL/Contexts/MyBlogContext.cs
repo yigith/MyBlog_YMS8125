751af121e88dc8fa9749d94b055105cf0ab6c1eb
Yigit Haciefendioglu <yigith1@gmail.com>
2019-09-12 12:15:24 +0300

    Common eklendi, seed eklendi

--- MyBlog.DAL/Contexts/MyBlogContext.cs
+++ MyBlog.DAL/Contexts/MyBlogContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyBlog.Common.Security;
 using MyBlog.DAL.Config;
 using MyBlog.Domain.Entities;
+using MyBlog.Domain.Enums;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,6 +22,15 @@ namespace MyBlog.DAL.Contexts
             modelBuilder.ApplyConfiguration(new CategoryConfiguration());
             modelBuilder.ApplyConfiguration(new PostConfiguration());
             modelBuilder.ApplyConfiguration(new UserConfiguration());
+
+            modelBuilder.Entity<User>().HasData(new User
+            {
+                Id = 1,
+                UserName = "yigith1@gmail.com",
+                Email = "yigith1@gmail.com",
+                PasswordHash = HashHelper.HashPassword("Ankara1."),
+                UserType = UserType.Admin
+            });
         }
 
         public DbSet<Category> Categories { get; set; }
