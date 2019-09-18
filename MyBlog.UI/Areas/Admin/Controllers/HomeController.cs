c3c6855200691df9c0800ed1282ab084c3ddf40c
Yigit Haciefendioglu <yigith1@gmail.com>
2019-09-18 14:23:28 +0300

    Admin Area

---
+++ MyBlog.UI/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBlog.UI.Areas.Admin.Controllers
+{
+    // https://docs.microsoft.com/tr-tr/aspnet/core/mvc/controllers/areas?view=aspnetcore-2.2
+    [Area("Admin")]
+    public class HomeController : AdminBaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- MyBlog.UI/Startup.cs
+++ MyBlog.UI/Startup.cs
@@ -79,6 +79,10 @@ namespace MyBlog.UI
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                  );
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
